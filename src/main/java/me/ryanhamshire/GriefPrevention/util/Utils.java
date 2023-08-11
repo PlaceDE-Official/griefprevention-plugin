bb5bf8d519e979c1fca7bc73c8f46ed312a50301
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2023-08-11 02:46:09 +0200

    simpler code for test if item matches

--- src/main/java/me/ryanhamshire/GriefPrevention/util/Utils.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/util/Utils.java
@@ -14,11 +14,17 @@ public class Utils
     public static boolean checkHeldItem(ItemStack itemStack)
     {
         //if he's not holding the golden shovel anymore, do nothing
-        if (itemStack == null) return false;
-        ItemMeta heldItem = itemStack.getItemMeta();
-        ItemMeta toolMeta = GriefPrevention.instance.config_claims_modificationTool.getItemMeta();
-        if (!(heldItem.getDisplayName().equals(toolMeta.getDisplayName()) && heldItem.getCustomModelData() == toolMeta.getCustomModelData() && heldItem.isUnbreakable() == toolMeta.isUnbreakable()))
+        if (itemStack == null)
             return false;
-        return !heldItem.hasDisplayName() || !toolMeta.hasDisplayName() || heldItem.getDisplayName().equals(toolMeta.getDisplayName());
+        ItemMeta heldItemMeta = itemStack.getItemMeta();
+        if (heldItemMeta == null)
+            return false;
+        ItemMeta toolMeta = GriefPrevention.instance.config_claims_modificationTool.getItemMeta();
+        assert toolMeta != null;
+        return itemStack.getType() != GriefPrevention.instance.config_claims_modificationTool.getType()
+                && heldItemMeta.getCustomModelData() == toolMeta.getCustomModelData()
+                && heldItemMeta.isUnbreakable() == toolMeta.isUnbreakable()
+                && heldItemMeta.getDisplayName().equals(toolMeta.getDisplayName()
+        );
     }
 }
